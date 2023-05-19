9c30a7c3729fc3a4088b97d736a758dfad8e82be
Tembisa <ndziwenitembisa@gmail.com>
2023-05-18 17:08:41 -0700

    C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+/*using puts ()*/
+#include <stdio.h>
+
+int main ()
+{
+	/*string decleration*/
+	char Mystr[] = "Programming is like building a multilingual puzzle,\n";
+	
+	/*writin to stdout*/
+	puts(Mystr);
+
+	return 0;
+}
+
